329a262873c49aeb498c316685c6608c8b6ca5bd
Siddhesh <siddheshborse25@gmail.com>
2025-07-11 18:02:55 +0530

    cors option changed

--- constants/config.js
+++ constants/config.js
@@ -1,8 +1,22 @@
 const corsOptions = {
-    origin : ["http://localhost:5173","http://localhost:4173",process.env.CLIENT_URL],
-    methods : ["GET","POST","PUT","DELETE"],
-    credentials : true,
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      "https://remontadathecomeback.vercel.app", // hardcoded client URL for production
+    ];
 
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ["Set-Cookie"], // <-- this helps Safari with cookies
 };
+
 
 export {corsOptions};
